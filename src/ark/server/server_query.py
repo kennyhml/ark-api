ae2c7630eaaab71b39dbc2068d90f883717d4888
Kenny <106347478+kennyhml@users.noreply.github.com>
2023-03-02 22:07:36 +0100

    fixed server_query crashing querying day

--- src/ark/server/server_query.py
+++ src/ark/server/server_query.py
@@ -1,3 +1,4 @@
+from sqlite3 import Time
 import a2s  # type:ignore[import]
 import requests  # type:ignore[import]
 
@@ -6,7 +7,8 @@ from .server import Server
 
 def query(server: Server) -> None:
     """Queries a server, sets its IP, game port, query port, and status"""
-    print(f"Fetching server status for {server.name}...")
+    print(f"Querying {server.name}\n{'-' * (len(server.name) + 9)}")
+
     result = requests.get(
         f"https://api.battlemetrics.com/servers?filter[search]={server.name}&filter[features][2e079b9a-d6f7-11e7-8461-83e84cedb373]=true&filter"
     )
@@ -35,7 +37,7 @@ def query(server: Server) -> None:
     print(f"Server day: {server.day}")
 
     status = server.status = _get_server_status(server.ip, server.query_port)
-    print(f"Server status: {status}")
+    print(f"Server status: {status}\n{'-' * (len(server.name) + 9)}")
 
 
 def _find_relevant_data(server_name: str, data: dict) -> dict:
@@ -50,7 +52,7 @@ def _find_relevant_data(server_name: str, data: dict) -> dict:
 def _get_server_status(ip: str, port: str) -> str:
     """Queries a servers status by ip and port"""
     try:
-        data = a2s.info((ip, port))
+        data = a2s.info((ip, port), timeout=7)
         if data:
             return "Ok"
         return "Down"
@@ -60,6 +62,11 @@ def _get_server_status(ip: str, port: str) -> str:
 
 def _set_server_day(server: Server) -> None:
     assert server.ip and server.query_port, "Can't set day without IP"
-
-    data = a2s.rules((server.ip, server.query_port))
+    try:
+        data = a2s.rules((server.ip, server.query_port), timeout=7)
+    except TimeoutError:
+        print("Failed to query server rules. Server didnt respond!")
+        server.status == "Down"
+        return
+    
     server.day = data["DayTime_s"]
